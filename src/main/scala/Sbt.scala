ed24b015f1c5f1ae9749a862d6e4c4d2f71aa46d
Mateusz Paprocki <mattpap@gmail.com>
2014-09-23 10:53:40 +0200

    Don't implement a custom sbt logger

--- src/main/scala/Sbt.scala
+++ src/main/scala/Sbt.scala
@@ -3,7 +3,7 @@ package org.refptr.iscala
 import java.io.File
 
 import sbt.{
-    BasicLogger,Level,StackTrace,
+    ConsoleLogger,
     ModuleID,ModuleInfo,
     Resolver,MavenRepository,
     IvyPaths,InlineIvyConfiguration,
@@ -13,40 +13,14 @@ import sbt.{
     InlineConfiguration,UpdateConfiguration,UpdateOptions,
     UpdateLogging,UnresolvedWarningConfiguration}
 
-import ShowLines._
-
-object Logger extends BasicLogger {
-    def log(level: Level.Value, message: => String) {
-        if (atLevel(level))
-            log(level.toString, message)
-    }
-
-    def success(message: => String) {
-        log(Level.SuccessLabel, message)
-    }
-
-    def trace(t: => Throwable) {
-        val traceLevel = getTrace
-        log("trace", StackTrace.trimmed(t, traceLevel))
-    }
-
-    private def log(label: String, message: => String) {
-        synchronized {
-            message.split("\n").foreach(line => println(s"[$label] $line"))
-        }
-    }
-
-    def control(event: sbt.ControlEvent.Value, message: => String): Unit = ???
-    def logAll(events: Seq[sbt.LogEvent]): Unit = ???
-}
-
 object Sbt {
     val defaultDependencies: Seq[ModuleID] = Seq()
     val defaultResolvers: Seq[Resolver] = Seq(Resolver.sonatypeRepo("releases"))
+    val logger = ConsoleLogger()
 
     def resolve(dependencies: Seq[ModuleID], resolvers: Seq[Resolver]): Option[Seq[File]] = {
         val paths = new IvyPaths(new File("."), None)
-        val ivyConf = new InlineIvyConfiguration(paths, resolvers, Nil, Nil, false, None, Seq("sha1", "md5"), None, UpdateOptions(), Logger)
+        val ivyConf = new InlineIvyConfiguration(paths, resolvers, Nil, Nil, false, None, Seq("sha1", "md5"), None, UpdateOptions(), logger)
         val ivySbt = new IvySbt(ivyConf)
         val scalaVersion = Util.scalaVersion
         val binaryScalaVersion = CrossVersion.binaryScalaVersion(scalaVersion)
@@ -55,12 +29,13 @@ object Sbt {
         val settings = new InlineConfiguration(project, ModuleInfo("IScala"), dependencies, ivyScala=Some(ivyScala))
         val module = new ivySbt.Module(settings)
         val updateConf = new UpdateConfiguration(None, false, UpdateLogging.DownloadOnly)
-        val updateReport = IvyActions.updateEither(module, updateConf, UnresolvedWarningConfiguration(), Logger)
+        val updateReport = IvyActions.updateEither(module, updateConf, UnresolvedWarningConfiguration(), logger)
         updateReport match {
             case Right(report) =>
                 Some(report.toSeq.map { case (_, _, _, jar) => jar }.distinct)
             case Left(warning) =>
-                warning.lines.foreach(Logger.error(_))
+                import ShowLines._
+                warning.lines.foreach(logger.error(_))
                 None
         }
     }
