8cda66c7e3ea6b4e99da4db39503cb64ed2f8ac8
Mateusz Paprocki <mattpap@gmail.com>
2014-09-28 09:05:03 +0200

    Test generation of the string representation

--- src/test/scala/Interpreter.scala
+++ src/test/scala/Interpreter.scala
@@ -3,6 +3,13 @@ package tests
 
 import org.specs2.mutable.Specification
 
+object Plain {
+    def unapply(data: Data): Option[String] = data match {
+        case Data((MIME.`text/plain`, output)) => Some(output)
+        case _ => None
+    }
+}
+
 class InterpreterSpec extends Specification {
     sequential
 
@@ -14,15 +21,15 @@ class InterpreterSpec extends Specification {
         import Results._
 
         "support primitive values" in {
-            interpret("1") must beLike { case Output(Value(_, "Int", _), "", "") => ok }
-            interpret("1.0") must beLike { case Output(Value(_, "Double", _), "", "") => ok }
-            interpret("\"XXX\"") must beLike { case Output(Value(_, "String", _), "", "") => ok }
+            interpret("1") must beLike { case Output(Value(_, "Int", Plain("1")), "", "") => ok }
+            interpret("1.0") must beLike { case Output(Value(_, "Double", Plain("1.0")), "", "") => ok }
+            interpret("\"XXX\"") must beLike { case Output(Value(_, "String", Plain("XXX")), "", "") => ok }
         }
 
         "support function values" in {
-            interpret("() => 1") must beLike { case Output(Value(_, "() => Int", _), "", "") => ok }
-            interpret("(x: Int) => x + 1") must beLike { case Output(Value(_, "Int => Int", _), "", "") => ok }
-            interpret("(x: Int, y: Int) => x*y + 1") must beLike { case Output(Value(_, "(Int, Int) => Int", _), "", "") => ok }
+            interpret("() => 1") must beLike { case Output(Value(_, "() => Int", Plain("<function0>")), "", "") => ok }
+            interpret("(x: Int) => x + 1") must beLike { case Output(Value(_, "Int => Int", Plain("<function1>")), "", "") => ok }
+            interpret("(x: Int, y: Int) => x*y + 1") must beLike { case Output(Value(_, "(Int, Int) => Int", Plain("<function2>")), "", "") => ok }
         }
 
         "support printing" in {
