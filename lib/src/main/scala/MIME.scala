217612c26e3c64a3f85cc578eb40a3a01f26209e
Mateusz Paprocki <mattpap@gmail.com>
2014-10-02 23:15:09 +0200

    Make MIMEs case objects to give them nice properties

--- lib/src/main/scala/MIME.scala
+++ lib/src/main/scala/MIME.scala
@@ -3,13 +3,13 @@ package display
 
 sealed abstract class MIME(val name: String)
 object MIME {
-    object `text/plain` extends MIME("text/plain")
-    object `text/html` extends MIME("text/html")
-    object `text/markdown` extends MIME("text/markdown")
-    object `text/latex` extends MIME("text/latex")
-    object `application/json` extends MIME("application/json")
-    object `application/javascript` extends MIME("application/javascript")
-    object `image/png` extends MIME("image/png")
-    object `image/jpeg` extends MIME("image/jpeg")
-    object `image/svg+xml` extends MIME("image/svg+xml")
+    case object `text/plain` extends MIME("text/plain")
+    case object `text/html` extends MIME("text/html")
+    case object `text/markdown` extends MIME("text/markdown")
+    case object `text/latex` extends MIME("text/latex")
+    case object `application/json` extends MIME("application/json")
+    case object `application/javascript` extends MIME("application/javascript")
+    case object `image/png` extends MIME("image/png")
+    case object `image/jpeg` extends MIME("image/jpeg")
+    case object `image/svg+xml` extends MIME("image/svg+xml")
 }
