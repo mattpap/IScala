b6a95900b2fa28bd5ac6dbc0017c03418712cb39
Mateusz Paprocki <mattpap@gmail.com>
2014-10-21 13:04:52 +0200

    Split Utils.scala into utils/*Util.scala

---
+++ src/test/scala/utils/InterpreterUtil.scala
@@ -0,0 +1,36 @@
+package org.refptr.iscala
+package tests
+
+trait InterpreterUtil {
+    object Plain {
+        def unapply(data: Data): Option[String] = data match {
+            case Data((display.MIME.`text/plain`, output)) => Some(output)
+            case _ => None
+        }
+    }
+
+    object NoOutput {
+        def unapply[T](output: Output[T]): Option[T] = output match {
+            case Output(value, "", "") => Some(value)
+            case _ => None
+        }
+    }
+
+    // XXX: if (fork) ("", true) else (sys.props("java.class.path"), false)
+    protected val intp = new Interpreter(sys.props("java.class.path"), Nil, false)
+
+    def interpret(code: String): Output[Results.Result] = {
+        Capture.captureOutput {
+            code match {
+                case Magic(name, input, Some(magic)) =>
+                    magic(intp, input)
+                case Magic(name, _, None) =>
+                    Results.Error // s"ERROR: Line magic function `%$name` not found."
+                case "" =>
+                    Results.NoValue
+                case _ =>
+                    intp.interpret(code)
+            }
+        }
+    }
+}
